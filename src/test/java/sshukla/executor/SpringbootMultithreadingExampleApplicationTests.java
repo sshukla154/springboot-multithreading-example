3fc564f57d2c8279e85f2c02969ad3f94463e320
Seemant Shukla <seemant@techspire.nl>
2022-10-28 09:46:14 +0200

    Initial Commit

---
+++ src/test/java/sshukla/executor/SpringbootMultithreadingExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package sshukla.executor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMultithreadingExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
